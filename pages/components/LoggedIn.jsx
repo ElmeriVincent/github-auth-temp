82cb1d6d06827547d96fb4ac51ba653ded19213d
Elmeri Keitaanranta <ElmeriVincent@gmail.com>
2022-08-19 17:42:46 +0300

    theme changed

--- pages/components/LoggedIn.jsx
+++ pages/components/LoggedIn.jsx
@@ -6,17 +6,20 @@ export default function LoggedIn({ session }) {
     return (
         <div className='flex justify-center items-center h-screen flex-col'>
             <div>
-                <h1 className='text-[#ecfac6] text-2xl text-center mb-2'>Authenticated!</h1>
+                <h1 className='text-[#494949] font-bold text-2xl text-center mb-2'>Authenticated!</h1>
             </div>
-            <div className="p-6 max-w-sm mx-auto bg-[#1E1E1E] rounded-xl shadow-lg flex items-center space-x-4">
+            <div className="p-6 max-w-sm mx-auto bg-[#494949] rounded-xl shadow-lg flex items-center space-x-4">
                 <div>
                     <div className='flex items-center justify-center mb-2'>
                         <Image className='rounded-full' src={session.user.image} alt="User Image" width={128} height={128} />
                     </div>
                     <div className='text-center'>
-                        <h1 className="text-l text-gray-500 dark:text-gray-400">{session.user.name}</h1>
-                        <p className="text-gray-500 dark:text-gray-400">{session.user.email}</p>
-                        <button className="mt-2 px-4 py-1 text-sm text-white font-semibold rounded-full border border-white] hover:text-black hover:bg-[#ecfac6] hover:border-transparent" onClick={() => signOut()}>Sign Out</button>
+                        <h1 className="text-lg text-white">{session.user.name}</h1>
+                        <p className="text-white">{session.user.email}</p>
+                        <button
+                            className="mt-2 px-4 py-1 text-sm text-white font-semibold rounded-full border border-white] hover:text-black hover:bg-white hover:border-transparent"
+                            onClick={() => signOut()}>Sign Out
+                        </button>
                     </div>
                 </div>
             </div>
--- pages/components/LoggedOut.jsx
+++ pages/components/LoggedOut.jsx
@@ -1,24 +1,20 @@
 import React from 'react'
-import Image from 'next/image';
+
 import { signIn } from "next-auth/react"
 import { FaGithub } from 'react-icons/fa';
 
 export default function LoggedOut() {
     return (
-        <div className='flex justify-center items-center h-screen'>
-            <div className="p-6 max-w-sm mx-auto bg-[#1E1E1E] rounded-xl shadow-lg flex items-center space-x-4">
-                <div>
-                    <div className="text-xl font-medium text-white text-center">Authentication</div>
-                    <p className="text-sm font-normal text-gray-500 dark:text-gray-400">Choose your authentication method below</p>
-                    <ul className="my-4 space-y-3">
-                        <li>
-                            <a onClick={() => signIn()} className="flex items-center p-3 text-base font-bold text-gray-900 bg-gray-50 rounded-lg hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
-                                <FaGithub className='h-4' />
-                                <span className="flex-1 ml-3 whitespace-nowrap">Github</span>
-                                <span className="inline-flex items-center justify-center px-2 py-0.5 ml-3 text-xs font-medium text-gray-500 bg-gray-200 rounded dark:bg-gray-700 dark:text-gray-400">Popular</span>
-                            </a>
-                        </li>
-                    </ul>
+        <div className="mt-96 mx-4">
+            <h1 className="text-center text-4xl font-bold text-[#494949]">Authentication Template Using NextAuth!</h1>
+            <p className="mt-2 text-center text-xl font-semibold text-[#494949]">Just Click The Github Logo And Do As Instructed</p>
+            <div className='mt-10 flex items-center justify-center'>
+                <div className="p-4 max-w-sm mx-auto bg-[#6a97db] rounded-xl shadow-lg flex items-center">
+                    <div>
+                        <a onClick={() => signIn()} className="flex items-center p-2 rounded-lg  group hover:shadow bg-gray-700 hover:bg-gray-600 text-white cursor-pointer">
+                            <FaGithub className='text-6xl' />
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
